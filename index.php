2ed4dfff8e080f73f7c725d56bae65dfa49b5b42
Koketso Madise <koketsomadise9@gmail.com>
2020-10-04 11:39:45 +0200

    updated crud operations

--- index.php
+++ index.php
@@ -33,16 +33,14 @@
                         <h2 class="pull-left">Employees Details</h2>
                         <a href="create.php" class="btn btn-success pull-right">Add New Employee</a>
                     </div>
-
-                    
                     <?php
                     // Include config file
                     require_once "config.php";
                     
                     // Attempt select query execution
                     $sql = "SELECT * FROM employees";
-                    if($result = mysqli_query($link, $sql)){
-                        if(mysqli_num_rows($result) > 0){
+                    if($result = $mysqli->query($sql)){
+                        if($result->num_rows > 0){
                             echo "<table class='table table-bordered table-striped'>";
                                 echo "<thead>";
                                     echo "<tr>";
@@ -54,7 +52,7 @@
                                     echo "</tr>";
                                 echo "</thead>";
                                 echo "<tbody>";
-                                while($row = mysqli_fetch_array($result)){
+                                while($row = $result->fetch_array()){
                                     echo "<tr>";
                                         echo "<td>" . $row['id'] . "</td>";
                                         echo "<td>" . $row['name'] . "</td>";
@@ -70,19 +68,20 @@
                                 echo "</tbody>";                            
                             echo "</table>";
                             // Free result set
-                            mysqli_free_result($result);
+                            $result->free();
                         } else{
                             echo "<p class='lead'><em>No records were found.</em></p>";
                         }
                     } else{
-                        echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+                        echo "ERROR: Could not able to execute $sql. " . $mysqli->error;
                     }
- 
+                    
                     // Close connection
-                    mysqli_close($link);
+                    $mysqli->close();
                     ?>
                 </div>
             </div>        
         </div>
     </div>
 </body>
+</html>
